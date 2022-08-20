44501ced97d98b5ee787b954703b4153a54edb3b
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-20 14:49:18 +0900

    [Update] testの作成

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,26 +1,3 @@
 class Gate
-  STATIONS = [:umeda, :juso, :mikuni]
-  FARES = [160, 180]
 
-  def initialize(name)
-    @name = name
-  end
-
-  # 引数で渡された切符に自分の駅名を保存する
-  def enter(ticket)
-    ticket.stamp(@name)
-  end
-
-  # exitメソッドではさらに乗車駅と自分の駅名から運賃を割り出す。足りていればtrue 足りていなければfalse
-  def exit(ticket)
-    fare = calc_fare(ticket)
-    fare <= ticket.fare
-  end
-
-  def calc_fare(ticket)
-    from = STATIONS.index(ticket.stamped_at)
-    to = STATIONS.index(@name)
-    distance = to - from
-    FARES[distance - 1]
-  end
 end
--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,35 +1,14 @@
 require 'minitest/autorun'
-require_relative '../lib/gate'
-require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
-  def setup
-    @umeda = Gate.new(:umeda)
-    @juso = Gate.new(:juso)
-    @mikuni = Gate.new(:mikuni)
-  end
+  def test_from_umeda_to_mikuni_charge_missing
+    # 改札機オブジェクトの作成
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
 
-  def test_umeda_to_juso
+    # 160円の切符を購入して梅田で乗車し、三国で降りる
     ticket = Ticket.new(160)
-    @umeda.enter(ticket)
-    assert @juso.exit(ticket)
-  end
-
-  def test_umeda_to_mikuni_when_fare_is_not_enough
-    ticket = Ticket.new(160)
-    @umeda.enter(ticket)
-    refute @mikuni.exit(ticket)
-  end
-
-  def test_umeda_to_mikuni_when_fare_is_enough
-    ticket = Ticket.new(180)
-    @umeda.enter(ticket)
-    assert @mikuni.exit(ticket)
-  end
-
-  def test_juso_to_mikuni
-    ticket = Ticket.new(160)
-    @juso.enter(ticket)
-    assert @mikuni.exit(ticket)
+    umeda.enter(ticket)
+    assert_not mikuni.exit(ticket) #=> false
   end
 end
