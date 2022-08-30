c338b38e6f3bf22d7206c5cbdf50dcb75e34d581
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-30 21:01:41 +0900

    [Refercotring]

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,22 +1,5 @@
-def convert_length(value, from_unit, to_unit)
-  meter_value = case from_unit
-                when 'm' then
-                  value / 1
-                when 'ft' then
-                  value / 3.28
-                when 'in' then
-                  value / 39.37
-                end
-  puts meter_value
+def convert_length(value:, from_unit:, to_unit:)
+  units = { m: 1.0, ft: 3.28, in: 39.37 }
 
-  changed_value = case to_unit
-                  when 'm' then
-                    meter_value * 1
-                  when 'ft' then
-                    meter_value * 3.28
-                  when 'in' then
-                    meter_value * 39.37
-                  end
-
-  changed_value.round(2)
+  (value / units[from_unit] * units[to_unit]).round(2)
 end
--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -3,14 +3,18 @@ require_relative '../lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_from_1_meter_to_inch
-    assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 39.37, convert_length(value: 1, from_unit: :m, to_unit: :in)
   end
 
   def test_from_15_inch_to_meter
-    assert_equal 0.38, convert_length(15, 'in', 'm')
+    assert_equal 0.38, convert_length(value: 15, from_unit: :in, to_unit: :m)
   end
 
   def test_from_35000_ft_to_meter
-    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
+    assert_equal 10670.73, convert_length(value: 35000, from_unit: :ft, to_unit: :m)
+  end
+
+  def test_from_23_8_inch_to_meter
+    assert_equal 0.60, convert_length(value: 23.8, from_unit: :in, to_unit: :m)
   end
 end
