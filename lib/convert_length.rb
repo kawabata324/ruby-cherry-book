9ff660511e3830f5d67fac0e5c9f6029a776015a
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-30 21:05:07 +0900

    [Update] constants

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,5 +1,5 @@
-def convert_length(value:, from_unit:, to_unit:)
-  units = { m: 1.0, ft: 3.28, in: 39.37 }
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
 
-  (value / units[from_unit] * units[to_unit]).round(2)
+def convert_length(value:, from_unit:, to_unit:)
+  (value / UNITS[from_unit] * UNITS[to_unit]).round(2)
 end
