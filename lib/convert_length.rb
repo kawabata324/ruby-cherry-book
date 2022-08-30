49b1cafd6bfc1c1ec6f443b6993cb60bde464b19
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-30 20:42:18 +0900

    [Update] pass test

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,3 +1,22 @@
 def convert_length(value, from_unit, to_unit)
-  39.37
+  meter_value = case from_unit
+                when 'm' then
+                  value / 1
+                when 'ft' then
+                  value / 3.28
+                when 'in' then
+                  value / 39.37
+                end
+  puts meter_value
+
+  changed_value = case to_unit
+                  when 'm' then
+                    meter_value * 1
+                  when 'ft' then
+                    meter_value * 3.28
+                  when 'in' then
+                    meter_value * 39.37
+                  end
+
+  changed_value.round(2)
 end
--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -5,4 +5,12 @@ class ConvertLengthTest < Minitest::Test
   def test_from_1_meter_to_inch
     assert_equal 39.37, convert_length(1, 'm', 'in')
   end
+
+  def test_from_15_inch_to_meter
+    assert_equal 0.38, convert_length(15, 'in', 'm')
+  end
+
+  def test_from_35000_ft_to_meter
+    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
+  end
 end
