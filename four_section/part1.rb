88ee93444ba1e21c8db68d8bcd8fe87397de4b3f
kawabata <s1931115@s.konan-u.ac.jp>
2022-09-16 09:58:27 +0900

    :sparkles: moduleの基本を学習

--- four_section/part1.rb
+++ four_section/part1.rb
@@ -28,43 +28,153 @@
 # => [1,2]
 
 # 配列の特定の位置にある要素を削除したい際はdelete_atメソッドを使います
-a = [1, 2, 3]
-a.delete_at(0)
-p a
-
-# => [2,3]
-
-# 配列を使った多重代入
-# 右辺が少ない場合はnilが入る
-a, b, c = [1, 2]
-p a
-p b
-p c
+# a = [1, 2, 3]
+# a.delete_at(0)
+# p a
+#
+# # => [2,3]
+#
+# # 配列を使った多重代入
+# # 右辺が少ない場合はnilが入る
+# a, b, c = [1, 2]
+# p a
+# p b
+# p c
 
 # => 1
 # => 2
 # => nil
 
 # 左辺が多い場合ははみ出した値は切り捨て
-a, b = [1, 10, 100]
-p a
-p b
-
-# => 1
-# => 10
-
-# 配列の多重代入の有効的に使える例
-# divmod
-# 割り算の商と余りを配列として返す
-# 下記だと 14 / 3の 商と余りを配列として返している
-
-p 14.divmod(3)
-# => [4,2]
-
-quotient, remainder = 14.divmod(3)
-# こうして別々の変数に入れたほうが使いやすい
-p quotient
-p remainder
+# a, b = [1, 10, 100]
+# p a
+# p b
+#
+# # => 1
+# # => 10
+#
+# # 配列の多重代入の有効的に使える例
+# # divmod
+# # 割り算の商と余りを配列として返す
+# # 下記だと 14 / 3の 商と余りを配列として返している
+#
+# p 14.divmod(3)
+# # => [4,2]
+#
+# quotient, remainder = 14.divmod(3)
+# # こうして別々の変数に入れたほうが使いやすい
+# p quotient
+# p remainder
 
 # 4
 # 2
+
+# puts "\e[31mABC\e[0m"
+
+# モジュールの用途
+#　継承を使わずにクラスに対して共有の特異メソッドを追加する
+# クラス名や定数名の衝突を防ぐために名前空間を作る
+# 関数的メソッドを定義する
+# シングルトンオブジェクトのように扱って設定値を保持する
+
+# モジュールの定義
+
+# module モジュール名
+#  # モジュール定義（メソッド定義など）
+# end
+
+# helloメソッドをもつGreetAbleモジュール定義
+
+# module GreetAble
+#   def hello
+#     'hello'
+#   end
+# end
+
+# クラスとの相違
+# モジュールからインスタンスを作成できない
+# 他のモジュールやクラスを継承することはできない
+
+# モジュールが何の役にたつのかわからない
+# モジュールを利用したメソッド定義
+# include exclude
+
+# Rubyでは単一継承
+# 従って一つのクラスには一つのスーパークラスしか持てない
+
+# class Product
+#   def title
+#     log 'title is called'
+#     log 'A great movie'
+#   end
+#
+#   private
+#     def log(text)
+#       puts "[LOG] #{text}"
+#     end
+# end
+#
+# class User
+#   def name
+#     log "name is called"
+#     log "Alice"
+#   end
+#
+#   private
+#     def log(text)
+#       puts "[LOG] #{text}"
+#     end
+# end
+
+# product = Product.new
+# product.title
+# # => [LOG] title is called
+# # [LOG] A great movie
+# user = User.new
+# user.name
+# => [LOG] name is called
+# [LOG] Alice
+
+# ログを出力する private_methodは重複しています。
+# しかし、このように重複しているからといって継承の使用は避けるべき
+
+# 継承はつかないがログを出力するという共通の機能は持たせたい。そんな時に選択肢として上がるのがモジュールです
+# モジュールにログ出力用のメソッドを定義し、クラスでそのモジュールをincludeするとモジュールで定義したメソッドがインスタンスメソッドとして呼び出せるようになる
+
+# ログ出力用のメソッドを定義するモジュール
+module Loggable
+  private
+    def log(text)
+      puts "[LOG] #{text}"
+    end
+end
+
+class Product
+  include Loggable
+
+  def title
+    log 'title is called'
+    log 'A great movie'
+  end
+end
+
+class User
+  include Loggable
+
+  def name
+    log "name is called"
+    log "Alice"
+  end
+end
+
+product = Product.new
+product.title
+
+user = User.new
+user.name
+
+# このようにモジュールをクラスにincludeすることで
+# 継承関係に気にすることなく機能を追加することに成功しました
+# このようにincludeして機能を追加することをmixinと言います
+# mixin先は基本的にどのようなクラスでもOKです
+# このようにRubyはmixinを利用することで多重継承ににた仕組みを実現しています
